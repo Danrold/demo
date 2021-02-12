eaaeee62c154f3faac29a57b0082e9a6e3415ec5
Danrold <danrold@mail.ru>
2021-02-12 15:06:02 +0700

    Fix validation

--- src/main/java/com/example/demo/dto/EditRequestDTO.java
+++ src/main/java/com/example/demo/dto/EditRequestDTO.java
@@ -8,17 +8,23 @@ import java.util.Date;
 @Data
 public class EditRequestDTO {
 
+    @NotNull(message = " не должно быть пустым")
     @Pattern(regexp = "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}", message = " не соответствует формату UUID")
     private String id;
+
     @NotBlank(message = " не должно быть пустым")
     @Size(min = 3, max = 255, message = " должно содержать от 3 до 255 символов")
     private String name;
 
+    @NotNull(message = " не должно быть пустым")
     @FutureOrPresent(message = " должно содержать сегодняшнее число или дату, которая еще не наступила")
     private Date start;
+
+    @NotNull(message = " не должно быть пустым")
     @FutureOrPresent(message = " должно содержать сегодняшнее число или дату, которая еще не наступила")
     private Date end;
 
+    @NotNull(message = " не должно быть пустым")
     @Pattern(regexp = "^true$|^false$", message = " должен принимать только true или false")
     private String isActive;
 }
