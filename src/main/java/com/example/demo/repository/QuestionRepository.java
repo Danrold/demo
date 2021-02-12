e59a64daacfe8536e3db71ab42f94d0642aa73ad
Danrold <danrold@mail.ru>
2021-02-12 17:31:29 +0700

    Java doc to JPA Repositories

--- src/main/java/com/example/demo/repository/QuestionRepository.java
+++ src/main/java/com/example/demo/repository/QuestionRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
+/**
+ * JPA репозиторий предоставляющий api запросов к базе данных для объектов Question
+ */
+
 public interface QuestionRepository extends JpaRepository<Question, UUID> {
 
 }
--- src/main/java/com/example/demo/repository/SurveyRepository.java
+++ src/main/java/com/example/demo/repository/SurveyRepository.java
@@ -6,8 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.*;
 
+/**
+ * JPA репозиторий предоставляющий api запросов к базе данных для объектов Survey
+ */
+
 public interface SurveyRepository extends JpaRepository<Survey, UUID> {
 
+    /**
+     * @param sort Параметры для сортировки
+     * @return Список сущностей Survey согласно переданным параметрам
+     */
     List<Survey> findAll(Sort sort);
 
 }
