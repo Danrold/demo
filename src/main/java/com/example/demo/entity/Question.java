39ed8e73f75d0717c454130ad983f4c44e9affdb
Danrold <danrold@mail.ru>
2021-02-12 17:21:56 +0700

    Java doc to Entity classes

--- src/main/java/com/example/demo/entity/Question.java
+++ src/main/java/com/example/demo/entity/Question.java
@@ -7,29 +7,55 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 import java.util.UUID;
 
+/**
+ * Класс сущности Вопрос, соответствует таблице questions
+ * Используются аннотации Lombok для генерации конструктора без параметров, а так же геттеров и сеттеров
+ * @author Ivan Medvedev
+ */
+
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "questions")
 public class Question {
 
+    /**
+     * Идентификатор вопроса в формате UUID
+     * Генерируется средствами Hibernate
+     * Первичный ключ, уникален и не может быть null
+     */
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(name = "id", nullable = false, unique = true)
     private UUID id;
 
+    /**
+     * Текст вопроса
+     */
     @Column(name = "text", nullable = false)
     private String text;
 
+    /**
+     * Порядковй номер вопроса в опросе
+     */
     @Column(name = "index", nullable = false)
     private int index;
 
+    /**
+     * Ссылка на опрос, к которому принадлежит сущность
+     */
     @ManyToOne
     @JoinColumn(name = "survey", nullable = false)
     @JsonIgnore
     private Survey survey;
 
+    /**
+     * Конструктор класса
+     * @param text Текст вопроса
+     * @param index Порядковый номер вопроса
+     * @param survey Родительская сущность (опрос) для данного вопроса
+     */
     public Question(String text, int index, Survey survey) {
         this.text = text;
         this.index = index;
--- src/main/java/com/example/demo/entity/Survey.java
+++ src/main/java/com/example/demo/entity/Survey.java
@@ -6,33 +6,63 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 import java.util.*;
 
+/**
+ * Класс сущности Опрос, соответствует таблице surveys
+ * Используются аннотации Lombok для генерации конструктора без параметров, а так же геттеров и сеттеров
+ * @author Ivan Medvedev
+ */
+
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "surveys")
 public class Survey {
 
+    /**
+     * Идентификатор вопроса в формате UUID
+     * Генерируется средствами Hibernate
+     * Первичный ключ, уникален и не может быть null
+     */
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(name = "id", unique = true, nullable = false)
     private UUID id;
 
+    /**
+     * Название опроса
+     */
     @Column(name = "name", nullable = false)
     private String name;
 
+    /**
+     * Дата начала опроса
+     */
     @Column(name = "start_date", nullable = false)
     private Date startDate;
 
+    /**
+     * Дата окончания опроса
+     */
     @Column(name = "end_date", nullable = false)
     private Date endDate;
 
+    /**
+     * Флаг активности вопроса
+     */
     @Column(name = "is_active", nullable = false)
     private boolean isActive;
 
+    /**
+     * Список вопросов в текущем опросе
+     */
     @OneToMany(mappedBy = "survey", cascade = CascadeType.ALL)
     private List<Question> questions;
 
+    /**
+     * Конструктор класса. Параметры isActive, startDate и endDate задаются по умолчанию
+     * @param name Название опроса
+     */
     public Survey(String name) {
         this.name = name;
         isActive = false;
