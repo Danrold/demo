ccc4045dc0f487d153128a3dfe10c8171aebc575
Danrold <danrold@mail.ru>
2021-02-13 13:31:31 +0700

    Java-doc to Services Interface

--- src/main/java/com/example/demo/service/QuestionService.java
+++ src/main/java/com/example/demo/service/QuestionService.java
@@ -6,13 +6,34 @@ import javax.persistence.EntityNotFoundException;
 
 /**
  * Интерфейс описывающй методы для работы с вопросами
+ * @author Ivan Medvedev
  */
 
 public interface QuestionService {
 
+    /**
+     * Метод добавляет вопрос к опросу по указанному идентификатору
+     * @param surveyID Идентификатор опроса
+     * @param text Текст вопроса
+     * @return Обновленный опрос
+     * @throws EntityNotFoundException Бросается если опрос не был найден
+     */
     Survey addQuestion(String surveyID, String text) throws EntityNotFoundException;
 
+    /**
+     * Метод изменяет текст вопроса
+     * @param questionID Идентификатор вопроса
+     * @param text Новый текст вопроса
+     * @return Обновленный опрос
+     * @throws EntityNotFoundException Бросается если вопрос не был найден
+     */
     Survey editQuestion(String questionID, String text) throws EntityNotFoundException;
 
+    /**
+     * Метод удаляет вопрос по идентификатору
+     * @param questionID Идентификатор вопроса
+     * @return Обновленный опрос
+     * @throws EntityNotFoundException Бросается если вопрос не был найден
+     */
     Survey deleteQuestion(String questionID) throws EntityNotFoundException;
 }
--- src/main/java/com/example/demo/service/SurveyService.java
+++ src/main/java/com/example/demo/service/SurveyService.java
@@ -3,19 +3,45 @@ package com.example.demo.service;
 import com.example.demo.dto.EditRequestDTO;
 import com.example.demo.entity.Survey;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 /**
  * Интерфейс описывающий методы для работы с опросами
+ * @author Ivan Medvedev
  */
 public interface SurveyService {
 
-    List<Survey> getAll(String field, String isAscending);
+    /**
+     * Метод возвращает отсортированный список опросов
+     * @param field Поле для сортировки (доступные значения: name, startDate)
+     * @param isAscending Порядок сортировки. Если установлен флаг true сортировка выполняется по возрастанию, false - по убыванию
+     * @return Список опросов
+     * @throws RuntimeException Бросается если значение field не соответствует доступным значениям
+     */
+    List<Survey> getAll(String field, String isAscending) throws RuntimeException;
 
+    /**
+     * Метод создает новый опрос
+     * @param name Имя опроса
+     * @return Созданный опрос
+     */
     Survey create(String name);
 
-    Survey edit(EditRequestDTO editRequest);
+    /**
+     * Метод редактирует существующий опрос
+     * @see com.example.demo.dto.EditRequestDTO
+     * @param editRequest объект содержащий информацию для редактирования
+     * @return Обновленный опрос
+     * @throws RuntimeException Бросается если опрос с указанным идентификатором не был найден, а так же если дата окончания опроса укзана раньше даты начала опроса
+     */
+    Survey edit(EditRequestDTO editRequest) throws RuntimeException;
 
-    void delete(String id);
+    /**
+     * Метод удаляет существующий опрос
+     * @param id Идентификатор опроса
+     * @throws EntityNotFoundException Бросается если опрос с указанным идентификатором не был найден
+     */
+    void delete(String id) throws EntityNotFoundException;
 
 }
