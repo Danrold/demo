27029ce5af964097a7f14e04b0df52f8fea506fc
Danrold <danrold@mail.ru>
2021-02-13 13:46:49 +0700

    java doc to main

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main класс приложения
+ * @author Ivan Medvedev
+ */
+
 @SpringBootApplication
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
